8ddf4e99907c49830e928d44ea90df40a8289646
michycipher <michelleutomi@yahoo.com>
2025-10-24 13:07:25 +0100

    Landing Page UI and Navbar responsiveness

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Main from "./components/Main"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className="min-h-screen bg-white dark:bg-black text-black dark:text-white flex flex-col">
+        <div className="h-[7vh]">
+          <Navbar />
+        </div>
+        <div className="flex-1 ">
+          <Hero />
+        </div>
+        <Main />
+      </main>
+    </>
+  )
+}
+
+export default App
