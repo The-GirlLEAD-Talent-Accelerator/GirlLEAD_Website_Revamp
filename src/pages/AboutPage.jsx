468122802363898c4b33d0277738ea69f18c6563
Precious Ihire <preciousihire@gmail.com>
2026-04-10 18:43:00 +0100

    Added Navbar

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../components/Navbar";
 import WhoWeAre from "../components/aboutus/WhoWeAre";
 
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <WhoWeAre/>
       
     </>
