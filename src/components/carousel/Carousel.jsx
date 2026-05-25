b0ca723723e51743c4a23096abea56320d0dec75
Precious IHIRE <preciousihire@gmail.com>
2026-05-25 23:51:01 +0100

    Fixed Video issues

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,22 +3,33 @@ import { useCarousel } from "../../hooks/useCarousel";
 import CarouselDots from "./CarouselDots";
 import Button from "../common/Button";
 
-
 export default function Carousel({ images }) {
   const { index, next, prev } = useCarousel(images.length);
-
+  const current = images[index];
+  const isVideo = current.endsWith(".mp4");
 
   return (
     <div className="mb-12">
-      {/* Image Wrapper */}
+      {/* Media Wrapper */}
       <div className="relative rounded-2xl overflow-hidden">
-        <img
-          key={index}
-          src={images[index]}
-          alt="Carousel"
-          className="w-full h-[220px] sm:h-[380px] object-cover transition-all duration-700 ease-in-out animate-fade"
-        />
 
+        {isVideo ? (
+          <video
+            key={current}
+            src={current}
+            className="w-full h-[220px] sm:h-[380px] object-cover transition-all duration-700 ease-in-out animate-fade"
+            controls
+            playsInline
+            preload="metadata"
+          />
+        ) : (
+          <img
+            key={current}
+            src={current}
+            alt="Carousel"
+            className="w-full h-[220px] sm:h-[380px] object-cover transition-all duration-700 ease-in-out animate-fade"
+          />
+        )}
 
         {/* Controls */}
         <div className="absolute left-3 top-1/2 -translate-y-1/2 z-20">
@@ -41,7 +52,6 @@ export default function Carousel({ images }) {
           />
         </div>
       </div>
-
 
       {/* Dots BELOW carousel */}
       <CarouselDots count={images.length} activeIndex={index} />
